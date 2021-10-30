691a5017e29c940ff4faff5d0efa71536ec96b66
Niklas Schlögel <niklasschloegel@student.hs-rm.de>
2021-10-30 11:39:50 +0200

    triggering order

--- shop/lib/screens/cart_screen.dart
+++ shop/lib/screens/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/providers/cart.dart' show Cart;
+import 'package:shop/providers/orders.dart';
 import 'package:shop/widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
@@ -39,7 +40,13 @@ class CartScreen extends StatelessWidget {
                       backgroundColor: Theme.of(context).primaryColor,
                     ),
                     TextButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        Provider.of<Orders>(context, listen: false).addOrder(
+                          cart.items.values.toList(),
+                          cart.totalAmount,
+                        );
+                        cart.clear();
+                      },
                       child: Text(
                         "ORDER NOW",
                         style: TextStyle(
