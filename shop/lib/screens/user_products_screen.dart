9693c4465eab0cdd0fd14ce1f00d71db440ee71e
Niklas Schlögel <niklasschloegel@student.hs-rm.de>
2021-11-05 10:16:15 +0100

    implementing pull to refresh in user products screen

--- shop/lib/screens/user_products_screen.dart
+++ shop/lib/screens/user_products_screen.dart
@@ -8,6 +8,10 @@ import 'package:shop/widgets/user_product_item.dart';
 class UserProductsScreen extends StatelessWidget {
   static const routeName = "/user-products";
 
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false).fetchProcuts();
+  }
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
@@ -25,19 +29,22 @@ class UserProductsScreen extends StatelessWidget {
         ),
         drawer: AppDrawer(),
         body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.all(8),
-            child: ListView.builder(
-              itemBuilder: (_, i) {
-                final prod = productsData.items[i];
-                return Column(
-                  children: [
-                    UserProductItem(prod.id, prod.title, prod.imageUrl),
-                    if (i != productsData.items.length - 1) Divider()
-                  ],
-                );
-              },
-              itemCount: productsData.items.length,
+          child: RefreshIndicator(
+            onRefresh: () => _refreshProducts(context),
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: ListView.builder(
+                itemBuilder: (_, i) {
+                  final prod = productsData.items[i];
+                  return Column(
+                    children: [
+                      UserProductItem(prod.id, prod.title, prod.imageUrl),
+                      if (i != productsData.items.length - 1) Divider()
+                    ],
+                  );
+                },
+                itemCount: productsData.items.length,
+              ),
             ),
           ),
         ));
