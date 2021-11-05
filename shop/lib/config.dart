8397e67a87ac6f5c0338f6edd785dbafd38742e7
Niklas Schlögel <niklasschloegel@gmail.com>
2021-11-05 18:30:17 +0100

    updating config class

--- shop/lib/config.dart
+++ shop/lib/config.dart
@@ -1,4 +1,13 @@
+/// Config for firebase backend.
+///
+/// This Config class holds information for the Firebase backend.
+/// To run this app, you'd need to create an own Firebase project
+/// (see https://console.firebase.google.com/) and fill the variables with the
+/// needed values as described on the values.
 class Config {
-  static const serverUrl =
-      ""; // Please provide your own Firebase Realtime DB URL ;)
+  /// Holds the URL of a Firebase Realtime Database
+  static const serverUrl = "";
+
+  /// Holds the API Key (found unter Project settings/Web API Key)
+  static const apiKey = "";
 }
