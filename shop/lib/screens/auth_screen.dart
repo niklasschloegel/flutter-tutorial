ceb0db3b900f28cdec880efe05b9cd4f5942b5f3
Niklas Schlögel <niklasschloegel@gmail.com>
2021-11-09 14:36:25 +0100

    adding manual animation on auth screen

--- shop/lib/screens/auth_screen.dart
+++ shop/lib/screens/auth_screen.dart
@@ -80,7 +80,8 @@ class AuthCard extends StatefulWidget {
   _AuthCardState createState() => _AuthCardState();
 }
 
-class _AuthCardState extends State<AuthCard> {
+class _AuthCardState extends State<AuthCard>
+    with SingleTickerProviderStateMixin {
   final GlobalKey<FormState> _formKey = GlobalKey();
   AuthMode _authMode = AuthMode.Login;
   Map<String, String> _authData = {
@@ -92,12 +93,28 @@ class _AuthCardState extends State<AuthCard> {
   final _emailFocusNode = FocusNode();
   final _passwordFocusNode = FocusNode();
   final _passwordConfirmationFocusNode = FocusNode();
+  late AnimationController _controller;
+  late Animation<Size> _heightAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+    _heightAnimation = Tween<Size>(
+      begin: Size(double.infinity, 260),
+      end: Size(double.infinity, 320),
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+  }
 
   @override
   void dispose() {
     _emailFocusNode.dispose();
     _passwordFocusNode.dispose();
     _passwordConfirmationFocusNode.dispose();
+    _controller.dispose();
     super.dispose();
   }
 
@@ -155,9 +172,12 @@ class _AuthCardState extends State<AuthCard> {
   void _switchAuthMode() {
     if (_authMode == AuthMode.Login) {
       setState(() => _authMode = AuthMode.Signup);
+      _controller.forward();
     } else {
       setState(() => _authMode = AuthMode.Login);
+      _controller.reverse();
     }
+    _heightAnimation.addListener(() => setState(() {}));
   }
 
   @override
@@ -170,9 +190,8 @@ class _AuthCardState extends State<AuthCard> {
       ),
       elevation: 8.0,
       child: Container(
-        height: _authMode == AuthMode.Signup ? 320 : 260,
-        constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        height: _heightAnimation.value.height,
+        constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
         width: deviceSize.width * 0.75,
         padding: EdgeInsets.all(16.0),
         child: Form(
