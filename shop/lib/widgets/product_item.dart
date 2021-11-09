accdc353f6fbc4d1de967e49d07dfa53aa8f5798
Niklas Schlögel <niklasschloegel@gmail.com>
2021-11-09 15:08:02 +0100

    adding hero transition to product detail screen

--- shop/lib/widgets/product_item.dart
+++ shop/lib/widgets/product_item.dart
@@ -26,10 +26,14 @@ class ProductItem extends StatelessWidget {
                 ),
               ),
             ),
-            child: FadeInImage(
-              placeholder: AssetImage("assets/images/product-placeholder.png"),
-              image: NetworkImage(product.imageUrl),
-              fit: BoxFit.cover,
+            child: Hero(
+              tag: product.id,
+              child: FadeInImage(
+                placeholder:
+                    AssetImage("assets/images/product-placeholder.png"),
+                image: NetworkImage(product.imageUrl),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
         ),
