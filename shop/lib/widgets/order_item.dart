b16e92384c9c27c8b674065c0dcb917aec7cbedc
Niklas Schlögel <niklasschloegel@gmail.com>
2021-11-09 15:43:29 +0100

    adding slide animation to orders screen

--- shop/lib/widgets/order_item.dart
+++ shop/lib/widgets/order_item.dart
@@ -17,28 +17,35 @@ class OrderItem extends StatefulWidget {
 }
 
 class _OrderItemState extends State<OrderItem> {
+  static const _ANIMATION_DURATION = Duration(milliseconds: 150);
   var _expanded = false;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text("\$${widget.order.amount.toStringAsFixed(2)}"),
-            subtitle: Text(
-              DateFormat("dd.MM.yyyy HH:mm").format(widget.order.dateTime),
+    return AnimatedContainer(
+      duration: _ANIMATION_DURATION,
+      height:
+          _expanded ? min(widget.order.products.length * 20.0 + 110, 200) : 95,
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text("\$${widget.order.amount.toStringAsFixed(2)}"),
+              subtitle: Text(
+                DateFormat("dd.MM.yyyy HH:mm").format(widget.order.dateTime),
+              ),
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () => setState(() => _expanded = !_expanded),
+              ),
+              onTap: () => setState(() => _expanded = !_expanded),
             ),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () => setState(() => _expanded = !_expanded),
-            ),
-            onTap: () => setState(() => _expanded = !_expanded),
-          ),
-          if (_expanded)
-            Container(
+            AnimatedContainer(
+              duration: _ANIMATION_DURATION,
               padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: min(widget.order.products.length * 20.0 + 10, 100),
+              height: _expanded
+                  ? min(widget.order.products.length * 20.0 + 10, 100)
+                  : 0,
               child: ListView(
                 children: widget.order.products
                     .map(
@@ -65,7 +72,8 @@ class _OrderItemState extends State<OrderItem> {
                     .toList(),
               ),
             ),
-        ],
+          ],
+        ),
       ),
     );
   }
